d572cb25a8b50288d79f2815fafa86f8fa07fe89
Nolan Cain <nolan.cain@dal.ca>
2026-01-10 23:46:59 -0400

    update to demo script

--- examples/demo.py
+++ examples/demo.py
@@ -13,12 +13,12 @@ def fishing_boat() -> tuple[trimesh.Trimesh, float]:
     return mesh, mass
 
 def sailboat() -> tuple[trimesh.Trimesh, float]:
-    raise NotImplementedError("The sailboat mesh needs to be fixed")
-    # mesh = gzpy.core.load_mesh(os.path.join('examples', 'sample_data', 'sailboat-game-asset', 'source', 'MergedFull.obj'))
+    # raise NotImplementedError("The sailboat mesh needs to be fixed")
+    mesh = gzpy.core.load_mesh(os.path.join('examples', 'sample_data', 'sailboat-game-asset', 'source', 'MergedFull.obj'))
 
-    # mass = 5000
+    mass = 5000
 
-    # return mesh, mass
+    return mesh, mass
 
 def main():
     print("Select the model to analyze:\n1 - Fishing Boat\n2 - Sailboat")
